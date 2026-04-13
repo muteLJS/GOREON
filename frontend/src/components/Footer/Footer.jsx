a40a2c5c82f0ee9075fe2b399154f4f067a89ca0
mutelJS <muteljs@gmail.com>
2026-04-13 17:15:23 +0900

    chore : 헤더 푸터 - 모바일 기본세팅 완료

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,84 @@
+import "./Footer.scss";
+
+import FacebookIcon from "assets/footer/pc/facebook.svg";
+import YoutubeIcon from "assets/footer/pc/youtube.svg";
+import InstagramIcon from "assets/footer/pc/instagram.svg";
+import ArrowRightIcon from "assets/footer/pc/arrow-right.svg";
+
+import HomeIcon from "assets/footer/mobile/home.svg";
+import SearchIcon from "assets/footer/mobile/search.svg";
+import CartIcon from "assets/footer/mobile/cart.svg";
+import HamburgerIcon from "assets/footer/mobile/hamburger.svg";
+
+function Footer() {
+  const socialIcons = [
+    { id: 1, name: "facebook", src: FacebookIcon, link: "/" },
+    { id: 2, name: "youtube", src: YoutubeIcon, link: "/" },
+    { id: 3, name: "instagram", src: InstagramIcon, link: "/" },
+  ];
+
+  const mobileQuickLinks = [
+    { id: 1, name: "home", src: HomeIcon, link: "/" },
+    { id: 2, name: "search", src: SearchIcon, link: "/search" },
+    { id: 3, name: "cart", src: CartIcon, link: "/cart" },
+    { id: 4, name: "menu", src: HamburgerIcon, link: "/category" },
+  ];
+
+  return (
+    <footer className="footer">
+      <div className="footer__box1">
+        <div className="top-1">
+          <div className="footer__policy">
+            <p href="/">정책</p>
+            <span>/</span>
+            <p href="/">법적 정보</p>
+          </div>
+          <div className="footer__links">
+            <p href="/">이용약관</p>
+            <p href="/">개인정보 처리방침</p>
+            <p href="/">쿠키 정책</p>
+          </div>
+        </div>
+
+        <div className="footer__company">
+          <p className="footer__company-title">회사정보</p>
+          <p>회사명 : GOREON</p>
+          <p>대표자 : GOREON</p>
+          <p>사업자 번호 : 02-0000-001</p>
+          <p>이메일 : abcd123@gmail.com</p>
+        </div>
+
+        <a href="/" className="footer__support">
+          <strong className="footer__support-number">1577-1577</strong>
+          <div className="go-cs">
+            <span className="footer__support-text">고객센터 바로가기</span>
+            <img src={ArrowRightIcon} alt="고객센터 바로가기" />
+          </div>
+        </a>
+
+        <div className="copyright">
+          <p className="footer__copyright">COPYRIGHT © GOREON. ALL RIGHTS RESERVED.</p>
+          <p className="footer__notice">본 플랫폼은 상품중개 역할만을 지원합니다.</p>
+        </div>
+
+        <div className="footer__social">
+          {socialIcons.map((icon) => (
+            <a href={icon.link} key={icon.id} className="footer__social-link">
+              <img src={icon.src} alt={icon.name} />
+            </a>
+          ))}
+        </div>
+      </div>
+
+      <div className="footer__box2">
+        {mobileQuickLinks.map((item) => (
+          <a href={item.link} key={item.id} className="footer__mobile-link" aria-label={item.name}>
+            <img src={item.src} alt={item.name} />
+          </a>
+        ))}
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ frontend/src/components/more/More.jsx
@@ -0,0 +1,7 @@
+import "./More.scss";
+
+function More() {
+  return <section className="more"></section>;
+}
+
+export default More;
