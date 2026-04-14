c663de40b687c6873f34f7949c54ae907f4928af
urusekai <uru.sekai@gmail.com>
2026-04-14 18:01:08 +0900

    Wip: 디자인 수정빼고 완성

--- frontend/src/components/ProductCard/ProductCardVertical.jsx
+++ frontend/src/components/ProductCard/ProductCardVertical.jsx
@@ -5,13 +5,13 @@ import Rating from "@/components/Rating/Rating";
 function ProductCardVertical({ product, action }) {
   return (
     <div className="product-card-vertical">
-      <img src={product.image} alt="노트북" />
+      <img className="product-card-vertical__thumbnail" src={product.image} alt="노트북" />
       <div className="product-card-vertical__title">{product.name}</div>
       <div className="product-card-vertical__rating">
         <Rating rating={product.rating} />
       </div>
       <div className="product-card-vertical__bottom">
-        <div className="product-card-vertical__price">{product.price}</div>
+        <div className="product-card-vertical__price">₩{product.price.toLocaleString("ko-KR")}</div>
         {action && <div className="product-card-vertical__action">{action}</div>}
       </div>
     </div>
