07d413f17a89ddf9881a5cbd06fdb134dc059611
urusekai <uru.sekai@gmail.com>
2026-04-15 14:47:24 +0900

    Chore: Modal 컴포넌트에 JSDoc 추가

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -1,9 +1,14 @@
-/* [컴포넌트] 모달창 (Modal)                                                             */
-/* 모바일에서 모달창을 그리는 컴포넌트 입니다. TOP/CONTENT/BOTTOM 으로 나누어져있습니다. */
-
+/* [컴포넌트] 모달창 (Modal)                   */                                           */
+/* 모바일에서 모달창을 그리는 컴포넌트 입니다. */
 import { useEffect } from "react";
 import "./Modal.scss";
 
+/**
+ * 모바일 화면의 하단에서 열리는 모달창 컴포넌트
+ * @param {string} title 모달 상단에 표시될 제목
+ * @param {React.ReactNode} children 모달 내부에 렌더링될 내용
+ * @param {Function} onClose 모달창 닫기 클릭할 때 실행될 함수
+ */
 function Modal({ title, children, onClose }) {
   useEffect(() => {
     document.body.style.overflow = "hidden";
