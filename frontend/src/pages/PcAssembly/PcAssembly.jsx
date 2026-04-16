0677a1cf2354c16962601632bb4f3c301b2d1bdb
kAzee0700 <yeom8951@gmail.com>
2026-04-16 15:41:25 +0900

    Revert "chore: sync pc assembly file"
    This reverts commit be5a69ad9991275859a5f99d4b353926251bdac6.

--- frontend/src/pages/PcAssembly/PcAssembly.jsx
+++ frontend/src/pages/PcAssembly/PcAssembly.jsx
@@ -3,21 +3,6 @@
 /* 설명: 사용 목적과 예산에 맞는 PC 견적을 안내하는 페이지입니다.             */
 /* -------------------------------------------------------------------------- */
 
-import { useState } from "react";
-import "./PcAssembly.scss";
-
-import ProductCardVertical from "@/components/ProductCard/ProductCardVertical";
-import ProductCardHorizontal from "@/components/ProductCard/ProductCardHorizontal";
-import Modal from "@/components/Modal/Modal";
-
-import banner1 from "@/assets/banner/banner-1.jpg";
-import ChevronDownIcon from "@/assets/icons/chevron-down.svg";
-import CheckIcon from "@/assets/icons/check.svg";
-import { PRODUCTS as productList } from "@/data/products";
-
-/* 필터 더미데이터 */
-const categories = ["CPU", "램", "메인보드", "그래픽카드", "저장장치", "케이스", "파워"];
-
 function PcAssembly() {
   return <main>Pc Assembly Page</main>;
 }
